6a3978282d4812980225c67acb2015beb461bd0f
BootesVoid <78485654+AMohamedAakhil@users.noreply.github.com>
2023-08-23 17:56:39 +0530

    vercel fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,12 +21,15 @@ const config = {
   plugins: ["@typescript-eslint"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+  '@typescript-eslint/no-explicit-any': 'off',
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
         prefer: "type-imports",
         fixStyle: "inline-type-imports",
       },
+      
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-misused-promises": [
