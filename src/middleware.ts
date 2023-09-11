826a81940173b7f8b83ba27925bcd0f1a90bf9f8
BootesVoid <78485654+AMohamedAakhil@users.noreply.github.com>
2023-09-11 16:22:25 +0530

    fix vercel deploy trpc error, making /api/trpc ignoredRoute, plan to use protectedProcedure for admin use

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,8 +6,7 @@ import { prisma } from "./server/db";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/api/webhooks/user", "/"],
-  ignoredRoutes: ["/((?!api|trpc))(_next|.+\..+)(.*)", "/api/trpc"]
+  publicRoutes: ["/api/webhooks/user", "/", "/api/trpc/category.getCategories", "/api/trpc/category.getSubCategories", "/api/trpc/category.getAllSubCategories"],
 });
 
 export const config = {
